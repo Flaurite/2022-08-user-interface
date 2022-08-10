cac55a92567ffa4e601e8fe75006e63a026413bd
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-08-10 12:11:06 +0400

    Complete

--- src/main/java/com/company/jmixpm/screen/main/MainScreen.java
+++ src/main/java/com/company/jmixpm/screen/main/MainScreen.java
@@ -1,18 +1,21 @@
 package com.company.jmixpm.screen.main;
 
+import com.company.jmixpm.app.TaskService;
+import com.company.jmixpm.entity.Project;
+import com.company.jmixpm.entity.Task;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.ScreenBuilders;
 import io.jmix.ui.ScreenTools;
-import io.jmix.ui.component.AppWorkArea;
-import io.jmix.ui.component.Button;
-import io.jmix.ui.component.Window;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.*;
 import io.jmix.ui.component.mainwindow.Drawer;
 import io.jmix.ui.icon.JmixIcon;
+import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.navigation.Route;
-import io.jmix.ui.screen.Screen;
-import io.jmix.ui.screen.Subscribe;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiControllerUtils;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 @UiController("MainScreen")
 @UiDescriptor("main-screen.xml")
@@ -29,6 +32,30 @@ public class MainScreen extends Screen implements Window.HasWorkArea {
     @Autowired
     private Button collapseDrawerButton;
 
+    @Autowired
+    private CollectionLoader<Project> projectsDl;
+
+    @Autowired
+    private CollectionLoader<Task> tasksDl;
+
+    @Autowired
+    private EntityComboBox<Project> projectSelector;
+    @Autowired
+    private TaskService taskService;
+
+    @Autowired
+    private DateField<LocalDateTime> dateSelector;
+    @Autowired
+    private TextField<String> nameSelector;
+
+    @Autowired
+    private Notifications notifications;
+
+    @Autowired
+    private MessageBundle messageBundle;
+
+    @Autowired
+    private ScreenBuilders screenBuilders;
 
     @Override
     public AppWorkArea getWorkArea() {
@@ -52,4 +79,56 @@ public class MainScreen extends Screen implements Window.HasWorkArea {
 
         screenTools.handleRedirect();
     }
+
+    @Subscribe("refreshTask")
+    public void onRefreshTask(Action.ActionPerformedEvent event) {
+        projectsDl.load();
+        tasksDl.load();
+    }
+
+    @Subscribe("addTask")
+    public void onAddTask(Action.ActionPerformedEvent event) {
+        if (projectSelector.getValue() == null
+                || nameSelector.getValue() == null
+                || dateSelector.getValue() == null) {
+            notifications.create()
+                    .withCaption(messageBundle.getMessage("validation.fieldsNotFilled.message"))
+                    .withType(Notifications.NotificationType.WARNING)
+                    .show();
+            projectSelector.focus();
+            return;
+        }
+
+        taskService.createTask(projectSelector.getValue(),
+                nameSelector.getValue(), dateSelector.getValue());
+
+        tasksDl.load();
+
+        projectSelector.setValue(null);
+        nameSelector.setValue(null);
+        dateSelector.setValue(null);
+    }
+
+    @Subscribe("tasksCalendar")
+    public void onTasksCalendarCalendarEventClick(Calendar.CalendarEventClickEvent<Task> event) {
+        Task task = (Task) event.getEntity();
+        if (task == null) {
+            return;
+        }
+
+        Screen screen = screenBuilders.editor(Task.class, this)
+                .editEntity(task)
+                .withOpenMode(OpenMode.DIALOG)
+                .build();
+
+        screen.addAfterCloseListener(closeEvent -> {
+            if (closeEvent.closedWith(StandardOutcome.COMMIT)) {
+                tasksDl.load();
+            }
+        });
+
+        screen.show();
+    }
+
+
 }
